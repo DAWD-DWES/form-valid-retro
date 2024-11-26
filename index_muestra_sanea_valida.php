ab04c8d57524358579ca74258dd050cb6ddfaa03
jivanrod <jivanrod@gmail.com>
2024-11-26 16:41:18 +0100

    Update index_muestra_sanea_valida.php

--- index_muestra_sanea_valida.php
+++ index_muestra_sanea_valida.php
@@ -85,8 +85,7 @@ if (filter_has_var(INPUT_POST, "enviar")) {
             <div class="flex-page">
                 <h1>Registro de cliente</h1>
                 <form class="capaform" name="registerform" 
-                      action="<?= $_SERVER['PHP_SELF'] ?>" method="POST" 
-                      enctype="multipart/form-data" novalidate>
+                      action="<?= $_SERVER['PHP_SELF'] ?>" method="POST" novalidate>
                     <div class="flex-outer">
                         <div class="form-section">
                             <label for="nombre">Nombre:</label>
@@ -157,10 +156,6 @@ if (filter_has_var(INPUT_POST, "enviar")) {
                         <div class="form-section">
                             <label for="suscripcion">Suscripción revista:</label>
                             <input id="suscripcion" type="checkbox"  name="suscripcion" /> 
-                        </div>
-                        <div class="form-section">
-                            <label for="foto">Foto:</label>
-                            <input id="foto" type="file" name="foto" accept=".jpg, .jpeg" />
                         </div>
                         <div class="form-section">
                             <div class="submit-section">
